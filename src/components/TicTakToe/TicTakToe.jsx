0e2c271a2e21f8e919556e162f3bd4c9ff16345b
Raj <rajdesigner025@gmail.com>
2023-10-11 17:34:46 +0530

    fixed errors

--- src/components/TicTakToe/TicTakToe.jsx
+++ src/components/TicTakToe/TicTakToe.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import './TicTakToe.css';
 
 import circle from "../Assets/circle.png";
 import cross from "../Assets/cross.png";
+
+// import circle from "../Assets/circle.png";
+// import cross from "../Assets/cross.png";
 
 let data =  ["","","","","","","","",""];
 
@@ -10,18 +13,30 @@ const TicTakToe = () => {
 
     let [count,setCount] = useState(0);
     let [lock,setLock] = useState(false);
+    let titleRef = useRef(null);
+    let box1 = useRef(null);
+    let box2 = useRef(null);
+    let box3 = useRef(null);
+    let box4 = useRef(null);
+    let box5 = useRef(null);
+    let box6 = useRef(null);
+    let box7 = useRef(null);
+    let box8 = useRef(null);
+    let box9 = useRef(null);
+
+    let box_array = [box1, box2, box3, box4, box5, box6, box7, box8, box9];
 
     const toggle = (e,num) => {
         if(lock){
             return 0;
         }
         if(count%2===0){
-            e.target.innerHTML = "<img src='$(circle)'>";
+            e.target.innerHTML = `<img src="${circle}" />`;
             data[num]='x';
             setCount(++count);
         }
         else{
-            e.target.innerHTML = "<img src='$(cross)' >" ;
+            e.target.innerHTML = `<img src='${cross}' >` ;
             data.num="o";
             setCount(++count);
         }
@@ -63,30 +78,44 @@ const TicTakToe = () => {
 
     const won = (winner) => {
         setLock(true);
+        if(winner==="x"){
+            titleRef.current.innerHTML = `Congratulation <img src=${cross} />`
+        }
+        else{
+            titleRef.current.innerHTML = `Congratulation <img src=${circle} />`
+        }
+    }
+
+    const reset = () => {
+        setLock(false);
+        titleRef.current.innerHTML = 'Tic Tac Toe in <span>React</span>';
+        box_array.map((e)=>{
+            e.current.innerHTML = "";
+        })
     }
 
     return(
         <div className="container">
-            <h1 className="title">Tic Tok Toe Game by <span>Raj</span></h1>
+            <h1 className="title" ref={titleRef}>Tic Tok Toe Game by <span>Raj</span></h1>
             <div className="board">
                 <div className="row1">
-                    <div className="boxes" onClick={(e)=>{toggle(e,0)}}></div>
-                    <div className="boxes"onClick={(e)=>{toggle(e,1)}}></div>
-                    <div className="boxes"onClick={(e)=>{toggle(e,2)}}></div>
+                    <div className="boxes" ref={box1} onClick={(e)=>{toggle(e,0)}}></div>
+                    <div className="boxes" ref={box2} onClick={(e)=>{toggle(e,1)}}></div>
+                    <div className="boxes" ref={box3} onClick={(e)=>{toggle(e,2)}}></div>
                 </div>
                 <div className="row1">
-                    <div className="boxes"onClick={(e)=>{toggle(e,3)}}></div>
-                    <div className="boxes"onClick={(e)=>{toggle(e,4)}}></div>
-                    <div className="boxes"onClick={(e)=>{toggle(e,5)}}></div>
+                    <div className="boxes" ref={box4} onClick={(e)=>{toggle(e,3)}}></div>
+                    <div className="boxes" ref={box5} onClick={(e)=>{toggle(e,4)}}></div>
+                    <div className="boxes" ref={box6} onClick={(e)=>{toggle(e,5)}}></div>
                 </div>
                 <div className="row1">
-                    <div className="boxes"onClick={(e)=>{toggle(e,6)}}></div>
-                    <div className="boxes"onClick={(e)=>{toggle(e,7)}}></div>
-                    <div className="boxes"onClick={(e)=>{toggle(e,8)}}></div>
+                    <div className="boxes" ref={box7} onClick={(e)=>{toggle(e,6)}}></div>
+                    <div className="boxes" ref={box8} onClick={(e)=>{toggle(e,7)}}></div>
+                    <div className="boxes" ref={box9} onClick={(e)=>{toggle(e,8)}}></div>
                 </div>
             </div>
-            <button className="reset">Reset</button>
+            <button className="reset" onClick={()=>{reset()}}>Reset</button>
         </div>
     )
 }
-export default TicTakToe
+export default TicTakToe;
